ac7d2199644dccefe1c99c04c86352e41212f7d4
Hitanshi1002 <hitanshibans@gmail.com>
2026-01-15 19:48:06 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express= require('express');
+const app=express();
+const path=require('path');
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/home', (req,res)=>{
+    res.render('home')
+})
+app.get('/', (req, res)=>{
+    res.send("Hello Camp Ground");
+})
+app.listen(3000,()=> {
+    console.log("Serving on port 3000!");
+})
